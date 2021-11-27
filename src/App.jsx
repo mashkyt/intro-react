b805b9ffe8a716a41acf4cbaf5393c3cd68ba040
mashkyt <ashikayt@outlook.com>
2021-11-27 16:53:01 +0800

    add a state variable for students

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ class App extends React.Component {
     this.state = {
       buttonClicked: "",
       assignments: [] /*Below this line, add the students state variable*/,
+      students: [],
       grades: {}
     };
 
@@ -56,7 +57,7 @@ class App extends React.Component {
           placeholder="Add Assignment..."
           currList={this.state.assignments}
           addFunction={this.addAssignment}
-          title="Assignments"
+          title="Assignments List"
         />
       );
     }
